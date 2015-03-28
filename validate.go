5eaf8b2f3a723465e4889d158774153622ae0b4e
ggobbe <guillaume.gobbe@gmail.com>
2015-03-28 22:04:03 +0000

    Whitespace before opening parenthese rule

--- validate.go
+++ validate.go
@@ -13,6 +13,7 @@ var (
 	startsWithSpaces   = regexp.MustCompile(`^\s+`)
 	fourSpacesIndented = regexp.MustCompile(`^([ ]{4}).[^\s]+`)
 	endsWithSpaces     = regexp.MustCompile(`\s+$`)
+	openingParenthese  = regexp.MustCompile(`[\(| ]\(`)
 )
 
 func validateFile(filename string) {
@@ -43,7 +44,7 @@ func validateFile(filename string) {
 func validateLine(line string) []string {
 	var warnings []string
 	if strings.Trim(line, " ") == "{" {
-		warnings = append(warnings, "First parenthese isn't on the end of the first line")
+		warnings = append(warnings, "First brace isn't on the end of the first line")
 	}
 
 	if endsWithSpaces.MatchString(line) {
@@ -52,6 +53,10 @@ func validateLine(line string) []string {
 
 	if startsWithSpaces.MatchString(line) && !fourSpacesIndented.MatchString(line) {
 		warnings = append(warnings, "Not indented using 4 spaces")
+	}
+
+	if strings.Contains(line, "(") && !openingParenthese.MatchString(line) {
+		warnings = append(warnings, "Opening parenthese not preceded by a whitespace")
 	}
 
 	return warnings
